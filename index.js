e9f5045f60ea2e374a90b67c7c60920539f10ba6
ThisGuh <jakiepolska1111@gmail.com>
2020-06-07 22:25:04 +0200

    Add keyboard handling

--- index.js
+++ index.js
@@ -7,4 +7,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.addEventListener('swipeUp', () => scroller.scroll(1))
     document.addEventListener('swipeDown', () => scroller.scroll(-1))
+    document.addEventListener('keydown', (event) => {
+        switch (event.code) {
+            case 'ArrowDown':
+                return scroller.scroll(1);
+            case 'ArrowUp':
+                return scroller.scroll(-1);
+            default:
+                return;
+        }
+    })
 })
